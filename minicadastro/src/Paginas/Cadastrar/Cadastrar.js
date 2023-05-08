4d666956774fb59e91fb0bbd799f559df7156696
codingscode <heronnestg@hotmail.com>
2023-05-07 22:09:31 -0300

    parte 17

--- minicadastro/src/Paginas/Cadastrar/Cadastrar.js
+++ minicadastro/src/Paginas/Cadastrar/Cadastrar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 
 
-
+//{"id": 1, "nome": "kyle", "sobrenome": "silva", "senha": "abc", "email": "kyle@gmail.com", "compras": [], "soma_total": 3600}
 
 const Cadastrar = () => {
    const [usuarioAtual, setUsuarioAtual] = useState({
@@ -14,25 +14,39 @@ const Cadastrar = () => {
       console.log(usuarioAtual)
    }
 
+   const handleSubmit = (e) => {
+      e.preventDefault()
+
+      const nome = e.target.elements.nome.value;
+      const sobrenome = e.target.elements.sobrenome.value;
+      const email = e.target.elements.email.value;
+      const senha = e.target.elements.senha.value;
+      const pagamento = e.target.elements.pagamento.value;
+
+      alert(`nome: ${nome}\nsobrenome: ${sobrenome}\nemail: ${email}\nsenha: ${senha}\npagamento: ${pagamento}`)
+   }
+
+//onChange={(e) => imprimirAtual(e.target.value, e.target.name)}
+
    return (
-      <div>
+      <form onSubmit={handleSubmit} >
          <div>Cadastrar Usuário</div>
-         <input name="nome" placeholder='nome' onChange={(e) => imprimirAtual(e.target.value, e.target.name)} /><br />
-         <input name="sobrenome" placeholder='sobrenome' onChange={(e) => imprimirAtual(e.target.value, e.target.name)} /><br />
-         <input name="senha" placeholder='senha' onChange={(e) => imprimirAtual(e.target.value, e.target.name)} /><br />
-         <input name="email" placeholder='email' onChange={(e) => imprimirAtual(e.target.value, e.target.name)} /><br />
+         <input id='nome' name="nome" placeholder='nome'  /><br />
+         <input id='sobrenome' name="sobrenome" placeholder='sobrenome'  /><br />
+         <input id='email' name="email" placeholder='email'  /><br />
+         <input id='senha' name="senha" placeholder='senha'  /><br />
          <div>
             selecionar produtos...
          </div>
          <div>
-            <select>
+            <select id='pagamento'>
                <option>selecione</option>
                <option>boleto</option>
                <option>cartão</option>
             </select>
          </div>
-         <button>Finalizar</button>
-      </div>
+         <button type='submit' >Finalizar</button>
+      </form>
    )
  
 }
