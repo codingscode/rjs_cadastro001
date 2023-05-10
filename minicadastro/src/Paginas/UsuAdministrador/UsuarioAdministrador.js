46d73ecdc45d2a4b08c4c17c8b09922c6e0ed381
codingscode <heronnestg@hotmail.com>
2023-05-10 11:13:32 -0300

    parte 26

--- minicadastro/src/Paginas/UsuAdministrador/UsuarioAdministrador.js
+++ minicadastro/src/Paginas/UsuAdministrador/UsuarioAdministrador.js
@@ -25,18 +25,21 @@ const UsuarioAdministrador = () => {
          <div className="uacabecalho" >Usuario Administrador</div>
          <div>
            <div className="uctitulo" >Lista de Usuários Comuns</div>
-           {usuarios.length ? usuarios.map((cada, i) => {
-              return (
-                 <div key={i} className='container' >
-                    <div>Nome: {cada.nome}</div>
-                    <div>Sobrenome: {cada.sobrenome}</div>
-                    <div>Email: {cada.email}</div>
-                    <div>Senha: {cada.senha}</div>
-                    <div>Pagamento: {cada.pagamento}</div>
-                    <button onClick={() => apagar(usuarios, cada.nome)} >apagar {cada.nome} </button>
-                 </div>
-              )
-            }) : 'Não há usuários cadastrados.'}
+           <div>
+              {usuarios.length ? usuarios.map((cada, i) => {
+                 return (
+                    <div key={i} className='container' >
+                       <div>Nome: {cada.nome}</div>
+                       <div>Sobrenome: {cada.sobrenome}</div>
+                       <div>Email: {cada.email}</div>
+                       <div>Senha: {cada.senha}</div>
+                       <div>Pagamento: {cada.pagamento}</div>
+                       <button onClick={() => apagar(usuarios, cada.nome)} >apagar {cada.nome} </button>
+                    </div>
+                 )
+               }) : <div className='aviso' >Não há usuários cadastrados.</div>}
+
+           </div>
          </div>
       </div>
    )
